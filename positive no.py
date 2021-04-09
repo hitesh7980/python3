1cc3f2f639b3c8c323ea1bb61c087ff69ed3b43c
hitesh7980 <82155559+hitesh7980@users.noreply.github.com>
2021-04-09 16:39:51 +0530

    Add files via upload

---
+++ positive no.py
@@ -0,0 +1,12 @@
+#print positive numbers from a list
+list1 = [12, -7, 5, 64, -14]
+for num in list1:
+    if num >= 0:
+        print(num, end=", ")
+print("=output")
+
+list2 = [12, 14, -93, 3]
+for num1 in list2:
+    if num1 >= 0:
+        print(num1,end=", ")
+print("=output")
